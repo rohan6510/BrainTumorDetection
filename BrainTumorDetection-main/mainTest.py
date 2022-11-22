e07daf018e8563629126a8596e8f5624d0a99275
rohan6510 <85016409+rohan6510@users.noreply.github.com>
2022-11-22 20:17:55 +0530

    Add files via upload

---
+++ BrainTumorDetection-main/mainTest.py
@@ -0,0 +1,24 @@
+import cv2
+from keras.models import load_model
+from PIL import Image
+import numpy as np
+
+model=load_model('BrainTumor10EpochsCategorical.h5')
+
+image=cv2.imread('C:\\Users\\sbynd\\Desktop\\Brain\\pred\\pred0.jpg')
+
+
+img=Image.fromarray(image)
+
+img=img.resize((64,64))
+
+img=np.array(img)
+
+input_img=np.expand_dims(img, axis=0)
+
+result=model.predict(input_img)
+print(result)
+
+
+
+
